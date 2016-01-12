6bdcc3eb1f3ce1728badd6ceb2ea83a30acb850e
Yansen <Yansen@test.com>
2016-01-12 17:15:10 +0800

    log echostr

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     var regex = /echostr=([^\&]*)/.exec(req.url);
     if(regex != null){
-    	res.write(regex[1]);
+    	var echoString = regex[1];
+    	console.log(echostr);
+    	res.write(echostr);
 	}
     res.end();
 
