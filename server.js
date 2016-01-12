6e0de9387e21724d0b53a4482e6178fc7de32bc5
Yansen <Yansen@test.com>
2016-01-12 13:37:11 +0800

    Add request url log

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 var port = 18080;
 http.createServer(function(req, res) {
+    console.log(new Date() + ": request comes");
+    console.log(req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
     res.end('<p>Hello World -11111</p>');
+
 }).listen(port);
