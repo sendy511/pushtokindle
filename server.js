1df00a03c10241c7121c5a4ed1350818ad7bbce7
Yansen <Yansen@test.com>
2016-01-12 17:12:36 +0800

    return echostr

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var port = 18080;
 http.createServer(function(req, res) {
-    console.log(new Date() + ": request comes");
-    console.log(req.url);
+    console.log(new Date() + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Node.js</h1>');
-    res.end('<p>Hello World -11111</p>');
+    var regex = /echostr=([^\&]*)/.exec(req.url);
+    if(regex != null){
+    	res.write(regex[1]);
+	}
+    res.end();
 
 }).listen(port);
