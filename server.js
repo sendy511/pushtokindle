69ac6236740dffca230cd75291a66e00182fbe1b
Yansen <Yansen@test.com>
2016-01-12 17:51:01 +0800

    should use body

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var port = 18080;
 http.createServer(function(req, res) {
     console.log(new Date() + req.url);
-    console.log(new Date() + req.content);
+    console.log(new Date() + req.body);
     res.writeHead(200, {'Content-Type': 'text/html'});
     
     responseEchostr(req, res);
