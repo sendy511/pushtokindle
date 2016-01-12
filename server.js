f0910621c3220506018b5fe439821189cc731994
Yansen <Yansen@test.com>
2016-01-12 17:48:48 +0800

    log content

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ var http = require('http');
 var port = 18080;
 http.createServer(function(req, res) {
     console.log(new Date() + req.url);
+    console.log(new Date() + req.content);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    var regex = /echostr=([^\&]*)/.exec(req.url);
+    
+    responseEchostr(req, res);
+
+
+
+    res.end();
+
+}).listen(port);
+
+function responseEchostr(req, res){
+	var regex = /echostr=([^\&]*)/.exec(req.url);
     if(regex != null){
     	var echostr = regex[1];
     	console.log(echostr);
     	res.write(echostr);
 	}
-    res.end();
-
-}).listen(port);
+}
