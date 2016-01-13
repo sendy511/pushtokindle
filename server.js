48a7d4838edb34eb22044611cf476b5e3468d848
Yansen <Yansen@test.com>
2016-01-13 10:19:45 +0800

    remove const string of body

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ http.createServer(function(req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/html'});
 
+	var body = '';
+    
     if(req.method == 'POST'){
-    	var body = '';
-
     	req.on('data', function(data){
     		body += data;
     	});
@@ -22,7 +22,6 @@ http.createServer(function(req, res) {
     
     responseEchostr(req, res);
 
-    var body = '<xml><Content><![CDATA[http://mp.weixin.qq.com/s?__biz=MjM5MjY3OTgwMA==&mid=402584363&idx=1&sn=fcbe3c37f2e9f88fb1d5fce8f2355e95&scene=2&srcid=01123VGABSm4rQnGz0iazZX5&from=timeline&isappinstalled=0#wechat_redirect]]></Content></xml>';
     xml2js.parseString(body, function(error, result){
         console.log("JSON:" + JSON.stringify(result));
         var url = result.xml.Content;
