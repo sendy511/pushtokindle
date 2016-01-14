ce58d1a768da47d6c9801ededbb96e1bf68d0b06
Yansen <Yansen@test.com>
2016-01-14 20:41:13 +0800

    check job status

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var http = require('http');
 var xml2js = require('xml2js');
 var JSON = require('JSON');
+var fs = require('fs');
+var url = require('url');
 
 var port = 18080;
+
+var api_address = "api2.online-convert.com";
+var api_key = 'f0c315563b656b7d40101ac578fc289f';
 
 http.createServer(function(req, res) {
     console.log(new Date() + req.url);
@@ -13,7 +18,8 @@ http.createServer(function(req, res) {
 		// responseMessage(req);
   //   }
     
-    createEbookParsingJob("http://www.baidu.com");
+    //createEbookParsingJob("http://www.baidu.com");
+    whetherJobFinished("6d9b67ab-ba05-11e5-a6e2-002590d8633c");
 
     responseEchostr(req, res);
 
@@ -50,13 +56,12 @@ function responseMessage(req){
     });
 }
 
-function createEbookParsingJob(url){
-    var api_address = "api2.online-convert.com";
-    var api_key = 'f0c315563b656b7d40101ac578fc289f';
+function createEbookParsingJob(content_url){
+    
     var post_data = JSON.stringify({
         'input': [{
             'type': 'remote',
-            'source': url
+            'source': content_url
         }],
         'conversion': [{
             'target': 'mobi'
@@ -81,10 +86,61 @@ function createEbookParsingJob(url){
             response_body += chunk;
         })
         res.on('end', function(){
-            console.log("Pushed command to create job for : " + url);
+            console.log("Pushed command to create job for : " + content_url);
             console.log(response_body);
+            if (response_body.contains("queued")){
+                var job_id = response_body.id;
+
+            }
+            
         })
     });
     post_request.write(post_data);
     post_request.end();
+}
+
+function whetherJobFinished(job_id){
+    var get_options = {
+        host: api_address,
+        path: "/jobs/" + job_id,
+        headers:{
+            'X-Oc-Api-Key': api_key,
+            'Content-Type': 'application/json',
+        }
+    };
+
+    http.get(get_options, function(res){
+        var job_status_body = '';
+        res.on('data', function(chunk){
+            job_status_body += chunk;
+        }).on('end', function(){
+            console.log("Get job status:" + job_status_body);
+            var job_status = JSON.parse(job_status_body);
+            if(job_status.status.code === 'completed'){
+                console.log("job finished : " + job_id);
+                var download_address = job_status.output[0].uri;
+                console.log("Get download uri: " + download_address);
+                downloadFile(download_address);
+            }
+            else{
+                console.log("Job " + job_id + " not finished yet, will try 3 seconds later");
+                setTimeout(whetherJobFinished(job_id), 3000);
+            }
+        });
+    });
+}
+
+function downloadFile(file_address){
+    var tmp_file = fs.createWriteStream("tmpfile");
+    http.get(file_address, function(res){
+        res.pipe(tmp_file);
+        tmp_file.on('finish', function(){
+            console.log("Write file into " + tmp_file);
+            tmp_file.close(sendEmail(tmp_file));
+        })
+    });
+}
+
+function sendEmail(file){
+    console.log("will send email");
 }
